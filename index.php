59d8dace54e1d7b09a0d81c08f4439145450d52f
snidhedinesh <snidhe2016@gmail.com>
2025-05-05 18:53:53 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,228 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index page</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Display&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Secular One' rel='stylesheet'>
+    <link rel="stylesheet" href="bootstrap/style.css">
+
+</head>
+<body>
+
+  <header class="navbar">
+    <div class="logo"><img src="bootstrap/images/logo-nav.png" alt=""></div>
+    <nav>
+      <ul class="nav-links">
+       
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Promotions</a></li>
+        <li><a href="#" class="btn">Login</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <div class="hero-content">
+      <p>INDIA’S TOP RATED CAR RENTAL SERVICE</p>
+      <h1>SERVICES ACROSS<br>2000+ CITIES</h1>
+
+      <div class="booking-container">
+    <div class="trip-type">
+      <button class="active">ONE WAY</button>
+      <button>ROUND TRIP</button>
+      <button>LOCAL</button>
+      <button>AIRPORT</button>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group">
+        <label for="from">FROM</label>
+        <input type="text" id="from" placeholder="Enter Pickup City">
+      </div>
+
+      <div class="form-group">
+        <label for="to">TO</label>
+        <input type="text" id="to" placeholder="Enter Drop City">
+      </div>
+
+      <div class="form-group">
+        <label for="pickup-date">PICK UP</label>
+        <input type="date" id="pickup-date" value="2025-01-30">
+      </div>
+
+      <div class="form-group">
+        <label for="pickup-time">PICK UP AT</label>
+        <input type="time" id="pickup-time" value="07:00">
+      </div>
+    </div>
+
+    <div class="explore-btn">
+      <button>EXPLORE CABS</button>
+    </div>
+  </div>
+    </div>
+  </section>
+  <section><div class="blue-div">
+    
+    <div class="row d-align">
+      <div class="col-lg-6 col-md-12 col-sm-12 text-center">
+<div class="position-relative">
+<img src="bootstrap/images/car-home.png" alt="" class="blue-div-img">
+<div class="dark-div">
+  <img src="bootstrap/images/dark-div.png" alt="">
+</div>
+
+</div>      </div>
+      <div class="col-lg-6 col-md-12 col-sm-12">
+        <div class="main-div">
+          <h4>India's Largest Intercity and Local travel Services</h4>
+          <p>We are Horizon travels, an online cab booking aggregator,
+providing customers with reliable and premium Intercity and Local car
+rental services. Over the last decade, we are uniquely placed as the
+largest chauffeur driven car rental company in India in terms of
+geographical reach.</p>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div></section>
+  <section>
+    <div class="container-div">
+      <div class="main-div">
+        <h2>our top services</h2></div>
+
+<div class="service-section">
+  <div class="service-card">
+    <img src="bootstrap/images/property-car.jpg" alt="">
+    <div>
+      <h4>Airport Transfers</h4>
+      <p>We care about your flight as much as you do. Our airport transfer services across airports in the country offer pickups and drops with complete reliability. Book in advance and rest easy - we will take care of the rest.</p>
+  <button>Book now</button>
+    </div>
+  </div>
+  <div class="service-card">
+    <img src="bootstrap/images/property-car.jpg" alt="">
+    <div>
+      <h4>Airport Transfers</h4>
+      <p>We care about your flight as much as you do. Our airport transfer services across airports in the country offer pickups and drops with complete reliability. Book in advance and rest easy - we will take care of the rest.</p>
+  <button>Book now</button>
+    </div>
+  </div>
+  <div class="service-card">
+    <img src="bootstrap/images/property-car.jpg" alt="">
+    <div>
+      <h4>Airport Transfers</h4>
+      <p>We care about your flight as much as you do. Our airport transfer services across airports in the country offer pickups and drops with complete reliability. Book in advance and rest easy - we will take care of the rest.</p>
+  <button>Book now</button>
+    </div>
+  </div>
+  <div class="service-card">
+    <img src="bootstrap/images/property-car.jpg" alt="">
+    <div>
+      <h4>Airport Transfers</h4>
+      <p>We care about your flight as much as you do. Our airport transfer services across airports in the country offer pickups and drops with complete reliability. Book in advance and rest easy - we will take care of the rest.</p>
+  <button>Book now</button>
+    </div>
+  </div>
+</div>
+      </div>
+   
+  </section>
+  <section><div class="blue-div">
+    <div class="main-div mt-3">
+      <h2>WHAT SETS Horizon travels APART?</h2>
+    </div>
+     <div class="travel-div">
+      <div class="travel-box">
+        <div><img src="bootstrap/images/home-icon-3.png" alt=""></div>
+        <h3>Clean and
+        Hygienic Car</h3></div>
+      <div class="travel-box">
+      <div><img src="bootstrap/images/home-icon-2.png" alt=""></div>
+      <h3>Transparent
+      Billing</h3></div>
+      <div class="travel-box">        <div><img src="bootstrap/images/home-icon-4.png" alt=""></div>
+      <h3>Expert
+      Chauffeurs</h3></div>
+
+      <div class="travel-box">        <div><img src="bootstrap/images/home-icon-1.png" alt=""></div>
+      <h3>2000+
+      cities</h3></div>
+
+
+     </div>
+  </section></div>
+  <section>
+    <div class="container-div">
+      <div class="main-div">
+        <h5>our top services</h5>
+        <p>To us, a road trip is one of the most exhilarating ways to travel the length and breadth of India. There's always something to look at, something to explore and to experience. Because we love travelling by road so much, we've been striving to make sure you have a great experience too. We wanted more of you to go on a road trip, and more of you to experience the same joys of travel that we do. Instead of driving, why not sit back and take our chauffeur driven cabs on your next vacation? We believe that the time you spend on your vacation should be entirely yours. So now, we are in 2000+ cities across India - to help you travel to wherever your heart desires. We love that you're free to stop to breathe in clean air, learn about cultures and taste local food when you travel by cabs. We love that these wholesome experiences make travelling better and enrich our lives. We live for the surprises we find on road trips. No city is too big or too small. We know travellers and wanderers like us are everywhere. You live near Khajuraho, you live near Aleppey, and you live near Alibag and near Tranquebar. We want you to visit them all. Pack your bags every weekend and explore everything there is to see around you. To make planning your vacation easier, you can book a cab with ease on our website, or call us on 0000000000 if you'd like to discuss your itinerary with our executives in detail. When you book an outstation cab with us, we'll send you a travel kit and help you plan your itinerary. We also have a handy cab booking app that will further reduce the hassles of booking a trip with us. Our expert drivers will guide you through some of the best experiences India has to offer. From the time you make a booking with us, to the time you get back home, we'll make sure you have a great road trip.</p>
+</div>
+<div class="main-div mt-3">
+        <h5>No matter where you travel - we've got a cab for you</h5>
+      <p>Planning a weekend getaway? Our outstation cab services will help you explore the best destinations, visit all the must-see places and taste the best local food. Did you just land at an airport or railway station closest to your destination? No problem! You can use our airport taxi, the transit pick up service to cover the last mile. We'll get you to your destination and show you some of the best sights along the way. Planning on traveling home for a family get-together? Try our newly introduced one-way cab services - no matter where you live, get dropped to your hometown by paying only one-side fare. Decided to take a personal day and spend the whole day exploring your city? Our local taxi packages will help you explore the best places to eat and drink at, some of the city's majestic monuments, greenest parks and oldest temples. You'll never have to worry about an empty travel itinerary again. Are you an offbeat traveller? Do you just hit the road and decide to take it from there? We offer one-way drops on several routes, in case you only want to be dropped to a destination and don't want to look back.</p></div>
+
+</section>
+<footer class="footer">
+    <div class="footer-container">
+      <div class="footer-column">
+       <div><img src="bootstrap/images/logo-footer.png" alt=""></div>
+        <p>We are company name Car Rentals, an online cab booking aggregator</p>
+        <div class="social-icons">
+          <a href="#"><i class="bi bi-facebook-f"></i></a>
+          <a href="#"><i class="bi bi-twitter"></i></a>
+          <a href="#"><i class="bi bi-youtube"></i></a>
+          <a href="#"><i class="bi bi-instagram"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-column">
+        <h4>GET IN TOUCH</h4>
+        <p>+91 123456789</p>
+        <p>Company@gmail.com</p>
+        <p>Company 123 Street D Block, 4th Floor</p>
+      </div>
+
+      <div class="footer-column">
+        <h4>QUICK LINKS</h4>
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Services</a>
+        <a href="#">Contact Us</a>
+      </div>
+
+      <div class="footer-column">
+        <h4>OUR FLEET</h4>
+        <a href="#">Luxury Car</a>
+        <a href="#">Sedan</a>
+        <a href="#">Vans</a>
+        <a href="#">Minivans</a>
+      </div>
+
+      <div class="footer-column">
+        <h4>POPULAR ROUTES</h4>
+        <a href="#">Pune to Nasik</a>
+        <a href="#">Pune to Mahabaleshwar</a>
+        <a href="#">Pune to Pandharpur</a>
+        <a href="#">Pune to Goa</a>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+      <p>© 2024 Developed By Snowfinch Consulting Group</p>
+      <div class="d-flex">
+        <a href="#">Terms & Conditions</a>
+        <a href="#">Privacy</a>
+        <a href="#">Cookies</a>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
